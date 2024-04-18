b83c3afd1b054a7823ca58f2571290790deba9e3
Christopher Bermudez <christopherbermudez98@gmail.com>
2024-04-17 23:21:33 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <template>
+      <h1 class="text-6xl font-extrabold">Hello world!</h1>
+    </template>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    // ...
+    '@nuxt/eslint',
+    // ...
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
